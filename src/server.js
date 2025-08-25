7cbc1c52b13ab2ecef49984d1f6dffc133dba26c
mateocasta33 <mateocasta33@gmail.com>
2025-08-25 13:17:13 -0500

    chat back

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const sequelize = require('./config/db.config');
 const cors = require('cors');
+const logger = require('morgan')
+const {Server} = require('socket.io')
+const {createServer} = require('node:http')
+const chatSocket = require("./sockets/chat.socket.js")
 
 const User = require('./models/user.model');
 const Profile = require('./models/profile.model');
@@ -23,11 +27,35 @@ app.use('/api', authRoutes);
 app.use('/api', profileRoutes);
 app.use('/api', socialRoutes);
 
+const server = createServer(app)
+
+const io = new Server(server, {
+    connectionStateRecovery: {
+        maxDisconnectionDuration // El tiempo maximo de desconexion
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('an user has connected')
+
+    socket.on('disconnect', () => {
+        console.log('an user has disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+chatSocket(io)
+
+app.use(logger('dev'))
+
 sequelize.sync({alter: true})
     .then(() => {
         console.log('Database synced');
         app.listen(PORT,() => {
-            console.log(`Server running in port${PORT}`);
+            console.log(`Server running in port http://localhost:${PORT}`);
         });
     })
     .catch(err => {
