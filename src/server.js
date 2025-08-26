cde5a1b8ff56572647c8f0f942e66b24467260e5
ZamirE27 <zaesco2001@gmail.com>
2025-08-26 12:59:22 -0500

    se corrigio errores del server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import sequelize from './config/db.config.js';
 import cors from 'cors';
+import {createServer} from 'node:http';
+import {Server} from 'socket.io';
+import chatSocket from './sockets/chat.socket.js';
+import logger from 'morgan';
 
 import User from './models/user.model.js';
 import Profile from './models/profile.model.js';
@@ -27,7 +31,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     connectionStateRecovery: {
-        maxDisconnectionDuration // El tiempo maximo de desconexion
+        // maxDisconnectionDuration // El tiempo maximo de desconexion
     }
 })
 
